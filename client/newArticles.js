5bf8cdea1c983b1d7f322f7cdd3efa3c0bb8c291
helb <helb@helb.cz>
2014-06-29 13:40:04 +0200

    sidebar fixes

---
+++ client/newArticles.js
@@ -0,0 +1,16 @@
+/*Template.newArticles.events({
+  'click a.article-link': function (event) {
+  	console.log('– EVENTS ––––––––––––––––––––––––––––––––––––––––––––');
+  	console.log('scrollTop: ' +  $(window).scrollTop());
+  	Session.set('scrollTop', $(window).scrollTop());
+  }
+});
+
+Template.newArticles.rendered = function () {
+	console.log('– RENDERED ––––––––––––––––––––––––––––––––––––––––––');
+	$("#sidebar").css({"margin-bottom": $(document).height() - $("#sidebar").height * 193) + "px"});
+	console.log('margin set to ' + $(document).height());
+	$(window).scrollTop(Session.get('scrollTop'));
+    console.log('scrolled to ' + Session.get('scrollTop'));
+};
+*/
