2a518a76d12fb2acdd547d5515b43a24af721648
helb <helb@helb.cz>
2014-07-14 21:43:15 +0200

    start archive from yesterday

--- client/archiv.js
+++ client/archiv.js
@@ -4,18 +4,19 @@ Template.archiv.events({
     since = new Date($('#form-archive-since').val());
     since.setHours(0,0,0,0);
     until = new Date($('#form-archive-until').val());
-    until.setHours(24,0,0,0);
+    until.setHours(0,0,0,0);
     Session.set('archiveSince', since);
     Session.set('archiveUntil', until);
   },
 });
 
 Template.archiv.rendered = function () {
-    $("html, body").animate({ scrollTop: 0 }, 600);
+  $("html, body").animate({ scrollTop: 0 }, 600);
   now = new Date();
-  weekAgo = new Date(now.getTime() - (24*60*60*1000) * 3);
+  yesterday = new Date(now.getTime() - (24*60*60*1000) * 1);
+  weekAgo = new Date(now.getTime() - (24*60*60*1000) * 4);
   console.log(now.toJSON().substr(0,10));
   console.log(weekAgo.toJSON().substr(0,10));
   $('#form-archive-since').val(weekAgo.toJSON().substr(0,10));
-  $('#form-archive-until').val(now.toJSON().substr(0,10));
+  $('#form-archive-until').val(yesterday.toJSON().substr(0,10));
 }
