df969f45a47ba925f03be7aea313ca205b60a27e
helb <helb@helb.cz>
2014-07-14 22:03:42 +0200

    use pikaday datepicker instead of [chrome-only :(] date input

--- client/archiv.js
+++ client/archiv.js
@@ -4,7 +4,7 @@ Template.archiv.events({
     since = new Date($('#form-archive-since').val());
     since.setHours(0,0,0,0);
     until = new Date($('#form-archive-until').val());
-    until.setHours(0,0,0,0);
+    until.setHours(24,0,0,0);
     Session.set('archiveSince', since);
     Session.set('archiveUntil', until);
   },
@@ -12,11 +12,18 @@ Template.archiv.events({
 
 Template.archiv.rendered = function () {
   $("html, body").animate({ scrollTop: 0 }, 600);
+i18n = {
+    previousMonth : 'Předchozí měsíc',
+    nextMonth     : 'Další měsíc',
+    months        : ['Leden','Únor','Březen','Duben','Květen','Červen','Červenec','Srpen','Září','Říjen','Listopad','Prosinec'],
+    weekdays      : ['Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'],
+    weekdaysShort : ['Ne','Po','Út','St','Čt','Pá','So']
+}
   now = new Date();
   yesterday = new Date(now.getTime() - (24*60*60*1000) * 1);
   weekAgo = new Date(now.getTime() - (24*60*60*1000) * 4);
-  console.log(now.toJSON().substr(0,10));
-  console.log(weekAgo.toJSON().substr(0,10));
-  $('#form-archive-since').val(weekAgo.toJSON().substr(0,10));
-  $('#form-archive-until').val(yesterday.toJSON().substr(0,10));
+  var pickerSince = new Pikaday({ field: document.getElementById('form-archive-since'), format: 'YYYY-MM-DD', defaultDate: weekAgo, setDefaultDate: true, firstDay: 1, i18n: i18n });
+  var pickerUntil = new Pikaday({ field: document.getElementById('form-archive-until'), format: 'YYYY-MM-DD', defaultDate: yesterday, setDefaultDate: true, firstDay: 1, i18n: i18n  });
+  // $('#form-archive-since').val(weekAgo.toJSON().substr(0,10));
+  // $('#form-archive-until').val(yesterday.toJSON().substr(0,10));
 }
