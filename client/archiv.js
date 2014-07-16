00275aea1cf10c64ec5d850ffe0a47777f2d665a
helb <helb@helb.cz>
2014-07-16 08:38:35 +0200

    archive shows today-6 to today-3 by default, code cleaned

--- client/archiv.js
+++ client/archiv.js
@@ -17,18 +17,13 @@ Template.archiv.events({
 
 Template.archiv.rendered = function () {
   $("html, body").animate({ scrollTop: 0 }, 600);
-i18n = {
+  i18n = {
     previousMonth : 'Předchozí měsíc',
     nextMonth     : 'Další měsíc',
     months        : ['Leden','Únor','Březen','Duben','Květen','Červen','Červenec','Srpen','Září','Říjen','Listopad','Prosinec'],
     weekdays      : ['Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'],
     weekdaysShort : ['Ne','Po','Út','St','Čt','Pá','So']
-}
-  now = new Date();
-  yesterday = new Date(now.getTime() - (24*60*60*1000) * 1);
-  weekAgo = new Date(now.getTime() - (24*60*60*1000) * 4);
-  var pickerSince = new Pikaday({ field: document.getElementById('form-archive-since'), format: 'YYYY-MM-DD', defaultDate: weekAgo, setDefaultDate: true, firstDay: 1, i18n: i18n });
-  var pickerUntil = new Pikaday({ field: document.getElementById('form-archive-until'), format: 'YYYY-MM-DD', defaultDate: yesterday, setDefaultDate: true, firstDay: 1, i18n: i18n  });
-  // $('#form-archive-since').val(weekAgo.toJSON().substr(0,10));
-  // $('#form-archive-until').val(yesterday.toJSON().substr(0,10));
+  }
+  pickerSince = new Pikaday({ field: document.getElementById('form-archive-since'), format: 'YYYY-MM-DD', defaultDate: Session.get('archiveSince'), setDefaultDate: true, firstDay: 1, i18n: i18n });
+  pickerUntil = new Pikaday({ field: document.getElementById('form-archive-until'), format: 'YYYY-MM-DD', defaultDate: Session.get('archiveUntil'), setDefaultDate: true, firstDay: 1, i18n: i18n });
 }
