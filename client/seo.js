a90cc2cbde3dbfa994bff94f84c600d3a81419ee
helb <helb@helb.cz>
2014-09-27 00:11:19 +0200

    update fallback og:image

--- client/seo.js
+++ client/seo.js
@@ -4,6 +4,6 @@ SEO.config({
     'description': 'Vše o kriminalitě v Plzni. Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'
   },
   og: {
-    'image': 'http://www.krimi-plzen.cz/images/style/biglogo.png'
+    'image': 'https://farm4.staticflickr.com/3921/15159532768_129e15feb2_o.jpg'
   }
 });
