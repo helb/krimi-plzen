34c3fc74fa0a9e5de0e96d352b6462caf415fc34
helb <helb@helb.cz>
2014-09-27 00:45:27 +0200

    replace ms-seo with blaze-meta

--- client/seo.js
+++ client/seo.js
@@ -1,9 +1,6 @@
-SEO.config({
-  title: 'Krimi Plzeň',
-  meta: {
-    'description': 'Vše o kriminalitě v Plzni. Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'
-  },
-  og: {
-    'image': 'https://farm4.staticflickr.com/3921/15159532768_129e15feb2_o.jpg'
+Meta.config({
+  options: {
+    title: "Krimi Plzeň",
+    suffix: "Krimi Plzeň"
   }
 });
