dd685048e54077cd69faccfdc3af8c83a38c6721
helb <helb@helb.cz>
2014-12-11 03:52:16 +0100

    remove unused photogalley styling

--- client/client.js
+++ client/client.js
@@ -1,18 +1,5 @@
 // infinite scrolling:
 $(window).scroll(function () {
-  if (document.getElementById("admin-article-list")) {
-    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-      if (!Session.get("currentLimit")) {
-        if (document.getElementById("new-articles")) {
-          Session.set("currentLimit", 10);
-        } else {
-          Session.set("currentLimit", 15);
-        }
-      }
-      Session.set("currentLimit", Session.get("currentLimit") + 5);
-      return Meteor.subscribe(Router.current().route.name, Session.get("currentLimit"));
-    }
-  }
   if (document.getElementById("archive-articles")) {
     var navHeight = $( window ).height() - 700;
              if ($(window).scrollTop() > navHeight) {
