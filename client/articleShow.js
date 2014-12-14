b61577ea2a0d14d8a53953f5c4b34ecf54f2e9a1
helb <helb@helb.cz>
2014-12-14 11:44:05 +0100

    empty photo container before adding a new photoset

--- client/articleShow.js
+++ client/articleShow.js
@@ -1,6 +1,5 @@
 Template.articleShow.helpers({
   flickr: function (set_id) {
-    $('#photos').empty();
     $.ajax({
       url: (window.location.protocol === 'https:' ?
           'https://secure' : 'https://api') +
@@ -14,9 +13,10 @@ Template.articleShow.helpers({
       dataType: 'jsonp',
       jsonp: 'jsoncallback'
     }).done(function (result) {
-      var linksContainer = $('#photos'),
+      var photoContainer = $('#photos'),
           baseUrl;
       if(result.photoset){
+        photoContainer.empty();
         $.each(result.photoset.photo, function (index, photo) {
           baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
           photo.server + '/' + photo.id + '_' + photo.secret;
@@ -24,7 +24,7 @@ Template.articleShow.helpers({
             .append($('<img>').prop('src', baseUrl + '_q.jpg').prop('alt', 'fotka'))
             .prop('href', baseUrl + '_b.jpg')
             .attr('class', 'swipebox')
-            .appendTo(linksContainer);
+            .appendTo(photoContainer);
         });
       }
     });
