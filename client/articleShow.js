11371aa45ad0793294c6566189b33606272b9109
helb <helb@helb.cz>
2015-02-22 15:06:00 +0100

    only load gallery if photoset id is set in current article

--- client/articleShow.js
+++ client/articleShow.js
@@ -3,36 +3,38 @@ Template.articleShow.rendered = function() {
 
   scrollToContent();
 
-  if (document.createStyleSheet) {
-    document.createStyleSheet(nanocss);
-  } else {
-    $("head").append($("<link rel='stylesheet' href='" + nanocss + "' type='text/css' media='screen' />"));
-  }
+  if($("#photos").length > 0){
+    if (document.createStyleSheet) {
+      document.createStyleSheet(nanocss);
+    } else {
+      $("head").append($("<link rel='stylesheet' href='" + nanocss + "' type='text/css' media='screen' />"));
+    }
 
-  $.getScript("https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/jquery.nanogallery.min.js")
-    .done(function() {
-      $("#photos").nanoGallery({
-        userID: '124879138@N07',
-        kind: 'flickr',
-        photoset: $("#photos").data("set"),
-        displayBreadcrumb: false,
-        locationHash:  false,
-        thumbnailLazyLoad: true,
-        lazyBuild: "display",
-        thumbnailWidth: 128,
-        thumbnailHeight: "auto",
-        thumbnailAlignment:   "left",
-        colorScheme: "light",
-        touchAnimation:  false,
-        thumbnailLabel:  {
-          display: false
-        },
-        viewerToolbar:  {
-          style: "stuckImage",
-          autoMinimize: 500,
-          standard: "previousButton, pageCounter, nextButton, playPauseButton, fullscreenButton, closeButton",
-          minimized: "minimizeButton, playPauseButton, closeButton",
-        }
-      });
-    })
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/jquery.nanogallery.min.js")
+      .done(function() {
+        $("#photos").nanoGallery({
+          userID: '124879138@N07',
+          kind: 'flickr',
+          photoset: $("#photos").data("set"),
+          locationHash:  false,
+          thumbnailLazyLoad: true,
+          lazyBuild: "display",
+          thumbnailWidth: 128,
+          thumbnailHeight: "auto",
+          thumbnailAlignment:   "left",
+          colorScheme: "light",
+          touchAnimation:  false,
+          thumbnailLabel:  {
+            display: false
+          },
+          viewerToolbar:  {
+            style: "stuckImage",
+            autoMinimize: 500,
+            standard: "previousButton, pageCounter, nextButton, playPauseButton, fullscreenButton, closeButton",
+            minimized: "minimizeButton, playPauseButton, closeButton",
+          },
+          displayBreadcrumb: false,
+        });
+      })
+  }
 };
