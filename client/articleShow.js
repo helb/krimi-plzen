ce67869c16b7f5d24e22900e209898e5005dd8d7
helb <helb@helb.cz>
2015-02-22 14:11:06 +0100

    remove unused flickr api code

--- client/articleShow.js
+++ client/articleShow.js
@@ -1,35 +1,3 @@
-Template.articleShow.helpers({
-  flickr: function(set_id) {
-    $.ajax({
-      url: (window.location.protocol === 'https:' ?
-          'https://secure' : 'https://api') +
-        '.flickr.com/services/rest/',
-      data: {
-        format: 'json',
-        method: 'flickr.photosets.getPhotos',
-        photoset_id:  set_id,
-        api_key: '7617adae70159d09ba78cfec73c13be3'
-      },
-      dataType: 'jsonp',
-      jsonp: 'jsoncallback'
-    }).done(function(result) {
-      if (result.photoset) {
-        var photoContainer = $('#photos'),
-          baseUrl;
-        photoContainer.empty();
-        $.each(result.photoset.photo, function(index, photo) {
-          baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
-            photo.server + '/' + photo.id + '_' + photo.secret;
-          $('<a/>')
-            .append($('<img/>').prop('src', baseUrl + '_q.jpg').prop('alt', photo.title))
-            .prop('href', baseUrl + '_b.jpg')
-            .appendTo(photoContainer);
-        });
-      }
-    });
-  }
-});
-
 Template.articleShow.rendered = function() {
   var nanocss = "https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/css/nanogallery.min.css";
 
