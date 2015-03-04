53217b3e0587f9e0f90665a2eb6978d2d209010c
Jiří Helebrant <jiri.helebrant@nic.cz>
2015-03-04 13:27:16 +0100

    don't use broken nanogallery breakpoints

--- client/articleShow.js
+++ client/articleShow.js
@@ -1,47 +1,51 @@
 Template.articleShow.rendered = function() {
-  var nanocss = "https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/css/nanogallery.min.css";
+	var nanocss =
+		"https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/css/nanogallery.min.css";
 
-  scrollToContent();
+	scrollToContent();
 
-  if ($("#photos").length > 0) {
-    if (document.createStyleSheet) {
-      document.createStyleSheet(nanocss);
-    } else {
-      $("head").append($("<link rel='stylesheet' href='" + nanocss + "' type='text/css' media='screen' />"));
-    }
+	if ($("#photos").length > 0) {
+		if (document.createStyleSheet) {
+			document.createStyleSheet(nanocss);
+		} else {
+			$("head").append($("<link rel='stylesheet' href='" + nanocss +
+				"' type='text/css' media='screen' />"));
+		}
 
-    $(document).ready(function() {
-      var setId = String($("#photos").data("set"));
-      $.getScript("https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/jquery.nanogallery.min.js")
-        .done(function() {
-          $("#photos").nanoGallery({
-            userID: '124879138@N07',
-            kind: 'flickr',
-            photoset: setId,
-            thumbnailLazyLoad: true,
-            locationHash: false,
-            thumbnailWidth: 128,
-            thumbnailHeight: "auto",
-            thumbnailAlignment:   "left",
-            colorScheme: "light",
-            touchAnimation:  false,
-            thumbnailLabel:  {
-              display: false
-            },
-            viewerToolbar:  {
-              style: "stuckImage",
-              autoMinimize: 500,
-              standard: "label, previousButton, pageCounter, nextButton, playPauseButton, fullscreenButton",
-              minimized: "label, minimizeButton, playPauseButton, closeButton",
-            },
-            displayBreadcrumb: false,
-            breakpointSizeSM: 480,
-            breakpointSizeME: 800,
-            breakpointSizeLA: 960,
-            breakpointSizeXL: 1100,
-            flickrSkipOriginal: false
-          });
-        })
-    });
-  }
+		$(document).ready(function() {
+			var setId = String($("#photos").data("set"));
+			$.getScript(
+					"https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/jquery.nanogallery.min.js"
+				)
+				.done(function() {
+					$("#photos").nanoGallery({
+						userID: '124879138@N07',
+						kind: 'flickr',
+						photoset: setId,
+						thumbnailLazyLoad:  true,
+						locationHash: false,
+						thumbnailWidth: 128,
+						thumbnailHeight: "auto",
+						thumbnailAlignment:   "left",
+						colorScheme: "light",
+						touchAnimation:  false,
+						thumbnailLabel:  {
+							display: false
+						},
+						viewerToolbar:  {
+							style: "stuckImage",
+							autoMinimize: 500,
+							standard: "label, previousButton, pageCounter, nextButton, playPauseButton, fullscreenButton",
+							minimized: "label, minimizeButton, playPauseButton, closeButton",
+						},
+						displayBreadcrumb: false,
+						breakpointSizeSM: 2000,
+						breakpointSizeME: 2000,
+						breakpointSizeLA: 2000,
+						breakpointSizeXL: 2000,
+						flickrSkipOriginal: false
+					});
+				})
+		});
+	}
 };
