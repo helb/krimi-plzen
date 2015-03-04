8cfaf3b9decf89791e58d3f45c7876395d8b2ced
Jiří Helebrant <jiri.helebrant@nic.cz>
2015-03-04 15:59:49 +0100

    base for custom photo viewer

--- client/articleShow.js
+++ client/articleShow.js
@@ -1,41 +1,62 @@
+Template.articleShow.helpers({
+  flickrGallery: function(set) {
+    $.ajax({
+      url: (window.location.protocol === 'https:' ?
+          'https://secure' : 'https://api') +
+        '.flickr.com/services/rest/',
+      data: {
+        format: 'json',
+        method: 'flickr.photosets.getPhotos',
+        photoset_id: set,
+        api_key: '7617adae70159d09ba78cfec73c13be3'
+      },
+      dataType: 'jsonp',
+      jsonp: 'jsoncallback'
+    }).done(function(result) {
+      if (result.photoset) {
+        var thumbContainer = $("#photos"),
+          baseUrl;
+        thumbContainer.empty();
+        $.each(result.photoset.photo, function(index, photo) {
+          baseUrl = 'http://farm' + photo.farm +
+            '.static.flickr.com/' +
+            photo.server + '/' + photo.id + '_' + photo.secret;
+          $('<a/>').addClass('grow')
+            .append($('<img>').prop('src', baseUrl + '_q.jpg').prop(
+              'alt', photo.title))
+            .prop('href', baseUrl + '_b.jpg')
+            .data('id', photo.id)
+            .appendTo(thumbContainer)
+            .click(function(e) {
+              $("#photoViewer").empty();
+              $("<a id='closeViewer'>&times;</a>").appendTo(
+                "#photoViewer").click(function(e) {
+                e.preventDefault();
+                $("#photoViewer").fadeOut();
+              });
+              e.preventDefault();
+              $("<img id='currentImage' src='" + $(this).attr(
+                  "href") +
+                "'>").appendTo("#photoViewer");
+              $(
+                "<nav id='viewerNav'><a id='navPrev'>&lt;</a><span id='currentPhotoIndex'>6</span>/<span id='totalPhotos'>" +
+                $("#photos a").length +
+                "</span><a id='navNext'>&gt;</a></nav>"
+              ).appendTo(
+                "#photoViewer")
+              $("#photoViewer").fadeIn();
+            });
+        });
+      }
+    });
+  }
+});
+
 Template.articleShow.rendered = function() {
-	var nanocss =
-		"https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.4.0/css/nanogallery.min.css";
+  $(document).ready(function() {
+    scrollToContent();
 
-	scrollToContent();
-
-	if ($("#photos").length > 0) {
-		if (document.createStyleSheet) {
-			document.createStyleSheet(nanocss);
-		} else {
-			$("head").append($("<link rel='stylesheet' href='" + nanocss +
-				"' type='text/css' media='screen' />"));
-		}
-
-		$(document).ready(function() {
-			var setId = String($("#photos").data("set"));
-			$("#photos").nanoGallery({
-				userID: '124879138@N07',
-				kind: 'flickr',
-				photoset: setId,
-				thumbnailLazyLoad:  true,
-				locationHash: false,
-				thumbnailWidth: 128,
-				thumbnailHeight: "auto",
-				thumbnailAlignment: "left",
-				colorScheme: "light",
-				touchAnimation:  false,
-				thumbnailLabel:  {
-					display: false
-				},
-				viewerToolbar:  {
-					style: "stuckImage",
-					autoMinimize: 500,
-					standard: "label, previousButton, pageCounter, nextButton, playPauseButton, fullscreenButton",
-					minimized: "label, minimizeButton, playPauseButton, closeButton",
-				},
-				displayBreadcrumb: false
-			});
-		});
-	}
-};
+    $("<div id='photoViewer'></div>").hide()
+      .appendTo($("body"));
+  });
+}
