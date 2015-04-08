89f1166e48ede44162cc46d629210315ee5f48b5
helb <helb@helb.cz>
2015-04-08 23:34:02 +0200

    use standard date input for archive

--- client/archiv.js
+++ client/archiv.js
@@ -1,11 +1,11 @@
 Template.archiv.events({
-  'click button#form-archive-submit': function (event) {
+  'click button#form-archive-submit': function(event) {
     event.preventDefault();
     since = new Date($('#form-archive-since').val());
-    since.setHours(0,0,0,0);
+    since.setHours(0, 0, 0, 0);
     until = new Date($('#form-archive-until').val());
-    until.setHours(23,59,59,999);
-    if(since>until){
+    until.setHours(23, 59, 59, 999);
+    if (since > until) {
       since = [until, until = since][0]; //swap variables
       pickerSince.setDate(since);
       pickerUntil.setDate(until);
@@ -15,15 +15,36 @@ Template.archiv.events({
   },
 });
 
-Template.archiv.rendered = function () {
-  $("html, body").animate({ scrollTop: 0 }, 600);
-  i18n = {
-    previousMonth : 'Předchozí měsíc',
-    nextMonth     : 'Další měsíc',
-    months        : ['Leden','Únor','Březen','Duben','Květen','Červen','Červenec','Srpen','Září','Říjen','Listopad','Prosinec'],
-    weekdays      : ['Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'],
-    weekdaysShort : ['Ne','Po','Út','St','Čt','Pá','So']
+Template.archiv.rendered = function() {
+  var d = new Date();
+  var day = d.getDate();
+  var month = d.getMonth() + 1;
+  var year = d.getFullYear();
+
+  var prevMonth = month - 1;
+  var prevYear = year;
+
+  if (prevMonth < 1) {
+    prevMonth = 12;
+    prevYear = year - 1;
   }
-  pickerSince = new Pikaday({ field: document.getElementById('form-archive-since'), format: 'YYYY-MM-DD', defaultDate: Session.get('archiveSince'), setDefaultDate: true, firstDay: 1, i18n: i18n });
-  pickerUntil = new Pikaday({ field: document.getElementById('form-archive-until'), format: 'YYYY-MM-DD', defaultDate: Session.get('archiveUntil'), setDefaultDate: true, firstDay: 1, i18n: i18n });
+
+  if (day < 10) {
+    day = '0' + day
+  }
+
+  if (month < 10) {
+    month = '0' + month
+  }
+
+  if (prevMonth < 10) {
+    prevMonth = '0' + prevMonth
+  }
+
+  $('#form-archive-since').val(year + "-" + prevMonth + "-" + day);
+  $('#form-archive-until').val(year + "-" + month + "-" + day);
+
+  $("html, body").animate({
+    scrollTop: 0
+  }, 600);
 }
