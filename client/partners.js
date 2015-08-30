f37c341373a8782bffb757ba8e161971e8fda7ff
helb <helb@helb.cz>
2015-08-30 19:54:29 +0200

    generate partner slider animation based on actual partner count, random partner order

--- client/partners.js
+++ client/partners.js
@@ -1,10 +1,22 @@
 Template.partners.helpers({
-  partners: function() {
-  	Meteor.subscribe("partnerList");
-    return Partners.find({}, {
-      sort: {
-        order: 1
-      }
-    });
-  },
+	partners: function() {
+		return _.shuffle(Partners.find({
+			is_enabled: true
+		}).fetch());
+	},
+	partnerSlideshow: function() {
+		Meteor.subscribe("partnerList");
+
+		var partnerCount = Partners.find().count();
+		var animation;
+		var fTime = (100 - partnerCount * 3) / partnerCount;
+		for (i = 0; i < partnerCount; i++) {
+			var position = i * 100 / partnerCount;
+			animation += position + "% {margin-left: " + i * -100 + "%} ";
+			animation += position + fTime + "% {margin-left: " + i * -100 + "%} ";
+		}
+		var sliderStyle = "#partners-slider {animation:partnerSlide " + partnerCount * 8 +
+						  "s ease-in-out infinite; width:" + partnerCount * 100 + "%;}";
+		return "<style>@keyframes partnerSlide{" + animation + "} " + sliderStyle + "</style>";
+	}
 });
