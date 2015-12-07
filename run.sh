a82f9f1847b4157d4ae3264eb5c132d7fb7b0236
helb <helb@helb.cz>
2015-12-08 00:01:32 +0100

    scripts to run production server and fetch js/css to static server root

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-export DDP_DEFAULT_CONNECTION_URL="http://ws.krimi-plzen.cz"
-export ROOT_URL="http://www.krimi-plzen.cz"
-
 while true; do
+    export DDP_DEFAULT_CONNECTION_URL="http://ws.krimi-plzen.cz"
+    #export ROOT_URL="http://www.krimi-plzen.cz"
     meteor run --settings settings.json --production;
 done
