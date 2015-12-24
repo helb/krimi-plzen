ab58703d517d1fbe30dae6357e823a71c7576138
helb <helb@helb.cz>
2015-12-24 15:49:07 +0100

    don't show author name

--- client/sidebar.js
+++
@@ -1,9 +0,0 @@
-Template.sidebar.helpers({
-    iframe: function() {
-        if (screen.width > 1100) {
-            return "<iframe src='http://www.ceskasit.cz/bazar' style='width: 0; height: 0; display: none;'></iframe>";
-        } else {
-            return "";
-        }
-    }
-});
