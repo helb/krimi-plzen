444b5f01ec1ffb30fac4d2bfb394f538ae199ee4
helb <helb@helb.cz>
2016-01-03 14:53:47 +0100

    remove PF

--- client/newArticles.js
+++ client/newArticles.js
@@ -6,14 +6,14 @@ Template.newArticles.events({
 
       var dateOffset = (24*60*60*1000) * Session.get("currentLimit");
       var daysAgo = new Date();
-      daysAgo.setTime(daysAgo.getTime() - dateOffset)
+      daysAgo.setTime(daysAgo.getTime() - dateOffset);
       daysAgo.setHours(0,0,0,0);
       var until = daysAgo;
       until.setHours(23,59,59,999);
       Session.set("archiveUntil", until);
       dateOffset = (24*60*60*1000) * 3;
       var since = daysAgo;
-      since.setTime(since.getTime() - dateOffset)
+      since.setTime(since.getTime() - dateOffset);
       Session.set("archiveSince", since);
 
       $("html, body").animate({ scrollTop: $('#new-articles .article-slug').last().offset().top - 20 }, 600);
