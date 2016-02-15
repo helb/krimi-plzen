5831d88c48e11da0ed73a6e90b066823fdfba1b5
helb <helb@helb.cz>
2016-02-15 23:31:42 +0100

    reformat rss feed

--- server/rss.js
+++ server/rss.js
@@ -1,182 +1,39 @@
-RssFeed.publish('clanky', function (query) {
-  var self = this;
-  self.setValue('title', self.cdata('Krimi Plzeň – články'));
-  self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
-  self.setValue('link', 'http://www.krimi-plzen.cz/');
-  self.setValue('lastBuildDate', new Date());
-  self.setValue('pubDate', new Date());
-  self.setValue('ttl', 1);
+RssFeed.publish('clanky', function(query) {
+    var self = this;
+    self.setValue('title', self.cdata('Krimi Plzeň – články'));
+    self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
+    self.setValue('link', 'http://www.krimi-plzen.cz/');
+    self.setValue('lastBuildDate', new Date());
+    self.setValue('pubDate', new Date());
+    self.setValue('ttl', 1);
 
-  Articles.find({}, {
-    sort: {
-      timestamp: -1
-    },
-    limit: 30
-  }).forEach(function (article) {
-    self.addItem({
-      "title": article.title,
-      "description": article.intro,
-      "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
-      "pubDate": article.timestamp,
-      "image": article.photo_url,
-      "media:content": article.photo_url
+    var filter = {is_published: true};
+
+    if (query.tag) {
+        filter.category = query.tag;
+    }
+
+    Articles.find(filter, {
+        sort: {
+            timestamp: -1
+        },
+        limit: 30
+    }).forEach(function(article) {
+        self.addItem({
+            "title": article.title,
+            "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
+            "pubDate": article.timestamp,
+            "guid": 'http://www.krimi-plzen.cz/a/' + article.slug,
+            "description": "<![CDATA[<div><a href='http://www.krimi-plzen.cz/a/" + article.slug + "'><img src='http:" + article.photo_url + "' alt='" + article.title + "'></a></div><div><h1><a href='http://www.krimi-plzen.cz/a/" + article.slug + "'>" + article.title + "</a></h1><p>" + article.intro + "</p></div>]]>"
+        });
     });
-  });
-});
-
-RssFeed.publish('zbrane', function (query) {
-  var self = this;
-  self.setValue('title', self.cdata('Krimi Plzeň – články o zbraních'));
-  self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
-  self.setValue('link', 'http://www.krimi-plzen.cz/zbrane');
-  self.setValue('lastBuildDate', new Date());
-  self.setValue('pubDate', new Date());
-  self.setValue('ttl', 1);
-  // managingEditor, webMaster, language, docs, generator
-
-  Articles.find({
-    'category': 'z'
-  }, {
-    sort: {
-      timestamp: -1
-    },
-    limit: 30
-  }).forEach(function (article) {
-    self.addItem({
-        "title": article.title,
-        "description": article.intro,
-        "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
-        "pubDate": article.timestamp,
-        "image": article.photo_url,
-        "media:content": article.photo_url
-    });
-  });
-});
-
-RssFeed.publish('pravni-minimum-ulice', function (query) {
-  var self = this;
-  self.setValue('title', self.cdata('Krimi Plzeň – Právní minimum ulice'));
-  self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
-  self.setValue('link', 'http://www.krimi-plzen.cz/pravni-minimum-ulice');
-  self.setValue('lastBuildDate', new Date());
-  self.setValue('pubDate', new Date());
-  self.setValue('ttl', 1);
-  // managingEditor, webMaster, language, docs, generator
-
-  Articles.find({
-    'category': 'pravni-minimum-ulice'
-  }, {
-    sort: {
-      timestamp: -1
-    },
-    limit: 30
-  }).forEach(function (article) {
-    self.addItem({
-        "title": article.title,
-        "description": article.intro,
-        "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
-        "pubDate": article.timestamp,
-        "image": article.photo_url,
-        "media:content": article.photo_url
-    });
-  });
-});
-
-RssFeed.publish('zachranari', function (query) {
-  var self = this;
-  self.setValue('title', self.cdata('Krimi Plzeň – Záchranáři'));
-  self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
-  self.setValue('link', 'http://www.krimi-plzen.cz/zachranari');
-  self.setValue('lastBuildDate', new Date());
-  self.setValue('pubDate', new Date());
-  self.setValue('ttl', 1);
-  // managingEditor, webMaster, language, docs, generator
-
-  Articles.find({
-    'category': 'zachranari'
-  }, {
-    sort: {
-      timestamp: -1
-    },
-    limit: 30
-  }).forEach(function (article) {
-    self.addItem({
-        "title": article.title,
-        "description": article.intro,
-        "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
-        "pubDate": article.timestamp,
-        "image": article.photo_url,
-        "media:content": article.photo_url
-    });
-  });
-});
-
-
-
-RssFeed.publish('hasici', function (query) {
-  var self = this;
-  self.setValue('title', self.cdata('Krimi Plzeň – Hasiči'));
-  self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
-  self.setValue('link', 'http://www.krimi-plzen.cz/hasici');
-  self.setValue('lastBuildDate', new Date());
-  self.setValue('pubDate', new Date());
-  self.setValue('ttl', 1);
-  // managingEditor, webMaster, language, docs, generator
-
-  Articles.find({
-    'category': 'hasici'
-  }, {
-    sort: {
-      timestamp: -1
-    },
-    limit: 30
-  }).forEach(function (article) {
-    self.addItem({
-        "title": article.title,
-        "description": article.intro,
-        "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
-        "pubDate": article.timestamp,
-        "image": article.photo_url,
-        "media:content": article.photo_url
-    });
-  });
-});
-
-
-RssFeed.publish('osoby', function (query) {
-  var self = this;
-  self.setValue('title', self.cdata('Krimi Plzeň – hledané osoby'));
-  self.setValue('description', self.cdata('Sledujeme veškeré dění v oblasti bezpečnostní situace, práce policistů, strážníků, záchranářů, hasičů nebo prostě jen lidí, kteří se nebojí pomoci jiným v nouzi.'));
-  self.setValue('link', 'http://www.krimi-plzen.cz/');
-  self.setValue('lastBuildDate', new Date());
-  self.setValue('pubDate', new Date());
-  self.setValue('ttl', 1);
-  // managingEditor, webMaster, language, docs, generator
-
-  Articles.find({
-    'category': 'o'
-  }, {
-    sort: {
-      timestamp: -1
-    },
-    limit: 30
-  }).forEach(function (article) {
-    self.addItem({
-        "title": article.title,
-        "description": article.intro,
-        "link": 'http://www.krimi-plzen.cz/a/' + article.slug,
-        "pubDate": article.timestamp,
-        "image": article.photo_url,
-        "media:content": article.photo_url
-    });
-  });
 });
 
 function escapeHtml(text) {
-  return text
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;");
+    return text
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
 }
