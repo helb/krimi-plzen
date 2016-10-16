edbe81b1f81a479401a1963b5ac372af924ca597
helb <helb@helb.cz>
2016-10-16 23:07:13 +0200

    ignore letter case when replacing gallery markup

--- krimiplzen/articles/templatetags/app_filters.py
+++ krimiplzen/articles/templatetags/app_filters.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.filter(name="insert_photo_thumbs")
 def insert_photo_thumbs(content):
-    urls = re.findall(r"<img src=[\"'](https?://[a-z0-9\-_/\.]+jpe?g)[\"'] ?(?:style=\"[^\"]*\")? ?/?>", content)
+    urls = re.findall(r"<img src=[\"'](https?://[a-z0-9\-_/\.]+jpe?g)[\"'] ?(?:style=\"[^\"]*\")? ?/?>", content, re.IGNORECASE)
     thumb_urls = []
     for url in urls:
         thumb_s = get_thumbnail(url, "100x100", crop="center", quality=95)
@@ -16,5 +16,5 @@ def insert_photo_thumbs(content):
         content = re.sub("<img src=[\"']" + url + "[\"'] ?(?:style=\"[^\"]*\")? ?/?>",
                          "<a href='" + url + "'>" +
                          "<img src='" + thumb_s.url + "' data-src=" + img_l.url + " srcset='" + thumb_s.url + " 100w, " + thumb_l.url + " 200w' data-srcset='" + img_s.url + " 640w, " + img_l.url + " 1440w' /></a>",
-                         content)
+                         content, re.IGNORECASE)
     return content
