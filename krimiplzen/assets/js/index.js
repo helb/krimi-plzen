ffc5973761f116d4841580284e31f854904670ab
helb <helb@helb.cz>
2016-10-18 08:27:52 +0200

    provisional partner slider

--- krimiplzen/assets/js/index.js
+++ krimiplzen/assets/js/index.js
@@ -1,1 +1,44 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const partnerSliderInner = document.querySelector(".article-list-partner-slider-inner");
+    const partnerSliderControls = document.querySelector(".article-list-partner-slider-controls");
+    const partnerCount = partnerSliderInner.children.length;
+    const slidePartners = function (position) {
+        console.log(position);
+        Array.from(partnerSliderControls.children).forEach((item) => {
+            item.classList.remove("current");
+        });
+        partnerSliderInner.style.transform = "translateX(" + (-100 / partnerCount * position) + "%)";
+        partnerSliderControls.children[position].classList.add("current");
+    };
 
+    for (let i = partnerCount; i >= 0; i--) {
+        const position = Math.random() * i | 0;
+        partnerSliderInner.appendChild(partnerSliderInner.children[position]);
+        partnerSliderControls.appendChild(partnerSliderControls.children[position]);
+    }
+
+    // for (let i = partnerCount; i >= 0; i--) {
+    //     console.log(partnerSliderControls.children[i]);
+    //     partnerSliderControls.children[i].setAttribute("data-position", i - 1); // .dataset.position = i;
+    // }
+    partnerSliderControls.children[0].classList.add("current");
+
+    let sliderPosition = 0;
+
+    const slideTimer = setInterval(function () {
+        sliderPosition += 1;
+        if (sliderPosition >= partnerCount) {
+            sliderPosition = 0;
+        }
+        slidePartners(sliderPosition);
+    }, 3000);
+
+    Array.from(partnerSliderControls.children).forEach((item, index) => {
+        item.dataset.position = index;
+        item.addEventListener("click", function (event) {
+            clearInterval(slideTimer);
+            event.stopPropagation();
+            slidePartners(parseInt(event.target.parentNode.dataset.position));
+        });
+    });
+});
