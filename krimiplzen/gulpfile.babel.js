7704fecd8dd832f8bc1cb2f9e084c22ca942c464
helb <helb@helb.cz>
2017-06-19 10:43:28 +0200

    Migrate frontend asset building to pure Gulp/Babel/SASS.

--- krimiplzen/gulpfile.babel.js
+++ krimiplzen/gulpfile.babel.js
@@ -1,93 +1,76 @@
 import gulp from "gulp";
-import webpack from "webpack-stream";
 import gutil from "gulp-util";
 import sass from "gulp-sass";
-import autoprefixer from "autoprefixer";
-import concat from "gulp-concat";
+import babel from "gulp-babel";
 import imagemin from "gulp-imagemin";
 import uglify from "gulp-uglify";
 import sourcemaps from "gulp-sourcemaps";
-import rename from "gulp-rename";
-import postcss from "gulp-postcss";
-import cssnano from "cssnano";
-import flexibility from "postcss-flexibility";
-import flexbugs from "postcss-flexbugs-fixes";
-import mqpacker from "css-mqpacker";
+import autoprefixer from "gulp-autoprefixer";
+import cssmin from "gulp-cssmin";
 
-const staticDir = "/home/helb/tmp/krimi_static/static/";
+const staticDir = gutil.env.static_dir || "/tmp/kp-static/";
 
-const mode = require("gulp-mode")({
-    modes: ["production", "development"],
-    default: "development"
-});
-
-const browserSync = require("browser-sync").create();
+const production = !!gutil.env.production;
 
 gulp.task("sass", () => {
-    const processors = [
-        autoprefixer({browsers: ["> 2%"]}),
-        // flexbugs(),
-        flexibility(),
-        mqpacker(),
-        cssnano()
-    ];
     gulp.src(["assets/styles/base.scss", "assets/styles/summernote.scss"])
         .pipe(sourcemaps.init())
         .pipe(sass().on("error", sass.logError))
-        .pipe(postcss(processors))
+        .pipe(autoprefixer({
+	            browsers:  ["last 2 versions", "safari >= 7", "ie >= 10"]
+	        }))
+        .pipe(production ? cssmin() : gutil.noop())
         .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest(staticDir + "css/"));
 });
 
-gulp.task("images", () => {
+gulp.task("optimize-images", () => {
     gulp.src(["assets/img/**/*"])
-        .pipe(mode.production(imagemin()))
-        .pipe(gulp.dest(staticDir + "img/"))
-        .pipe(mode.development(browserSync.reload({
-            stream: true
-        })));
+        .pipe(production ? imagemin() : gutil.noop())
+        .pipe(gulp.dest(staticDir + "img/"));
 });
 
-gulp.task("js", () => {
-    gulp.src("")
-        .pipe(webpack(require("./webpack.config.js")))
-        // .pipe(mode.production(uglify()))
-        .pipe(gulp.dest(staticDir + "js/"))
-        .pipe(mode.development(browserSync.reload({
-            stream: true
-        })));
+gulp.task("build-js", () => {
+    const files = [
+        "./assets/js/base.js",
+        "./assets/js/index.js",
+        "./assets/js/article.js"
+    ];
+    return gulp.src(files)
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(production ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest(staticDir + "js/"));
 });
+
 
 gulp.task("copy-js", () => {
     gulp
-    .src("assets/js/summernote-gallery-plugin.js")
-    .pipe(gulp.dest(staticDir + "js/"));
+        .src("assets/js/summernote-gallery-plugin.js")
+        .pipe(gulp.dest(staticDir + "js/"));
     gulp
-    .src("assets/js/summernote-krimilink-plugin.js")
-    .pipe(gulp.dest(staticDir + "js/"));
-    gulp
-    .src("assets/js/flexibility.js")
-    .pipe(gulp.dest(staticDir + "js/"));
-    gulp
-    .src("assets/js/polyfill.min.js")
-    .pipe(gulp.dest(staticDir + "js/"));
+        .src("assets/js/summernote-krimilink-plugin.js")
+        .pipe(gulp.dest(staticDir + "js/"));
 });
 
 gulp.task("robots", () => gulp
     .src("assets/robots.txt")
-    .pipe(gulp.dest(staticDir + "")));
+    .pipe(gulp.dest(staticDir + ""))
+);
 
-gulp.task("devserver", ["watch"], () => {
-    browserSync.init({
-        server: staticDir + ""
-    });
-});
+
+gulp.task("build", ["sass", "optimize-images", "build-js", "copy-js", "robots"]);
 
 gulp.task("watch", ["build"], () => {
-    gulp.watch("assets/styles/**/*.scss", ["sass"]);
-    gulp.watch("assets/img/**/*", ["images"]);
-    gulp.watch("assets/js/**/*.js", ["js", "copy-js"]);
+    gulp.watch("./assets/js/*.js", ["build-js", "copy-js"]);
+    gulp.watch("./assets/styles/**/*.scss", ["sass"]);
+    // gulp.watch("./krimiplzen/assets/**/*.{ttf,eot,woff}", ["copy-fonts"]);
+    gulp.watch("./assets/img/*.{svg,png,gif,jpg,jpeg}", ["optimize-images"]);
 });
 
-gulp.task("build", ["sass", "images", "js", "copy-js", "robots"]);
-gulp.task("default", ["watch"]);
+if (production) {
+    gulp.task("default", ["build"]);
+} else {
+    gulp.task("default", ["build", "watch"]);
+}
