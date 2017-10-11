75bb84514f3b50279bf0719a91a7161e5beb2af6
helb <helb@helb.cz>
2017-10-11 10:57:42 +0200

    inline the analytics script

--- krimiplzen/assets/js/base.js
+++ krimiplzen/assets/js/base.js
@@ -1,22 +1,4 @@
-(function (i, s, o, g, r, a, m) {
-    i["GoogleAnalyticsObject"] = r;
-    i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments);
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-})(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
-
-ga("create", "UA-52320835-1", "auto", {
-    "siteSpeedSampleRate": 20
-});
-
 document.addEventListener("DOMContentLoaded", function (event) {
-    ga("send", "pageview");
-
     if (typeof SVGUseElement !== "function") {
         document.querySelector(".site-logo img").style.display = "block";
     };
