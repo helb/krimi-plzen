2cf16d42b642a5a526b1722a2a83a25b83a6c0bc
Jiří Helebrant <jiri.helebrant@nic.cz>
2018-04-18 22:58:25 +0200

    Update to Babel 7, clean up Gulp config, add security.txt

--- krimiplzen/gulpfile.babel.js
+++ krimiplzen/gulpfile.babel.js
@@ -12,63 +12,71 @@ const staticDir = gutil.env.static_dir || "./static/";
 
 const production = !!gutil.env.production;
 
-gulp.task("sass", () => {
-    gulp.src([
-        "assets/styles/base.scss",
-        "assets/styles/sidebar.scss",
-        "assets/styles/article-list.scss",
-        "assets/styles/article.scss",
-        "assets/styles/summernote.scss"
-    ])
+gulp.task("sass", () =>
+    gulp
+        .src([
+            "assets/styles/base.scss",
+            "assets/styles/sidebar.scss",
+            "assets/styles/article-list.scss",
+            "assets/styles/article.scss",
+            "assets/styles/summernote.scss"
+        ])
         .pipe(sourcemaps.init())
         .pipe(sass().on("error", sass.logError))
-        .pipe(autoprefixer({
-	            browsers:  ["last 2 versions", "safari >= 7", "ie >= 10"]
-	        }))
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions", "safari >= 7", "ie >= 10"]
+            })
+        )
         .pipe(production ? cssmin() : gutil.noop())
         .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(staticDir + "css/"));
-});
+        .pipe(gulp.dest(staticDir + "css/"))
+);
 
-gulp.task("optimize-images", () => {
-    gulp.src(["assets/img/**/*"])
+gulp.task("optimize-images", () =>
+    gulp
+        .src(["assets/img/**/*"])
         .pipe(production ? imagemin() : gutil.noop())
-        .pipe(gulp.dest(staticDir + "img/"));
-});
+        .pipe(gulp.dest(staticDir + "img/"))
+);
 
-gulp.task("build-js", () => {
-    const files = [
-        "./assets/js/base.js",
-        "./assets/js/index.js",
-        "./assets/js/article.js"
-    ];
-    return gulp.src(files)
+gulp.task("build-js", () =>
+    gulp
+        .src([
+            "./assets/js/base.js",
+            "./assets/js/index.js",
+            "./assets/js/article.js"
+        ])
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(production ? uglify() : gutil.noop())
         .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(staticDir + "js/"));
-});
-
-
-gulp.task("copy-js", () => {
-    const files = [
-        "assets/js/summernote-gallery-plugin.js",
-        "assets/js/summernote-krimilink-plugin.js",
-        "assets/js/siema.min.js"
-    ];
-    gulp
-        .src(files)
-        .pipe(gulp.dest(staticDir + "js/"));
-});
-
-gulp.task("robots", () => gulp
-    .src("assets/robots.txt")
-    .pipe(gulp.dest(staticDir + ""))
+        .pipe(gulp.dest(staticDir + "js/"))
 );
 
+gulp.task("copy-js", () =>
+    gulp
+        .src([
+            "assets/js/summernote-gallery-plugin.js",
+            "assets/js/summernote-krimilink-plugin.js",
+            "assets/js/siema.min.js"
+        ])
+        .pipe(gulp.dest(staticDir + "js/"))
+);
 
-gulp.task("build", ["sass", "optimize-images", "build-js", "copy-js", "robots"]);
+gulp.task("copy-txt", () =>
+    gulp
+        .src(["assets/robots.txt", "assets/security.txt"])
+        .pipe(gulp.dest(staticDir + ""))
+);
+
+gulp.task("build", [
+    "sass",
+    "optimize-images",
+    "build-js",
+    "copy-js",
+    "copy-txt"
+]);
 
 gulp.task("watch", ["build"], () => {
     gulp.watch("./assets/js/*.js", ["build-js", "copy-js"]);
